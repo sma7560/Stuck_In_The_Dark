75bcc7b94b137faec12d6aca74204c0dd53b299b
sma7560 <sma7560@gmail.com>
2019-09-20 14:32:28 -0400

    Reset Stats now updates stats with current run if run was successful

--- StuckInTheDark/Assets/_Scripts/Stats.cs
+++ StuckInTheDark/Assets/_Scripts/Stats.cs
@@ -8,22 +8,26 @@ public class Stats : MonoBehaviour {
 
 	private Canvas end;
 	private Text currStats, bestStats, msg;
-	private GameObject statsPanel;
 
 	private Color victory, defeat;
+
+	private float endT;
+	private int endP, endB;
+
+	private Flashlight player;
 
 	// Use this for initialization
 	void Start () {
 		end = GetComponent<Canvas> ();
-		statsPanel = GameObject.FindGameObjectWithTag ("stats");
 		msg = GameObject.FindGameObjectWithTag ("endText").GetComponent<Text>();
 		currStats = GameObject.FindGameObjectWithTag ("currentRun").GetComponent<Text>();
 		bestStats = GameObject.FindGameObjectWithTag ("hiscores").GetComponent<Text>();
+		player = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Flashlight>();
 
 		end.enabled = false;
 
 		defeat = new Color(178/255f, 1/255f, 1/255f, 100/255f);
-		victory = new Color(1f, 1f, 1f, 100/255f);
+		victory = new Color(1/255f, 1f, 1/255f, 100/255f);
 	}
 
 	// Update is called once per frame
@@ -31,6 +35,10 @@ public class Stats : MonoBehaviour {
 	}
 
 	public void displayStats(float time, int endPower, int endBatteries, bool alive){
+		endT = time;
+		endP = endPower;
+		endB = endBatteries;
+
 		end.enabled = true;
 		msg.text = "YOU ESCAPED!";
 
@@ -89,14 +97,25 @@ public class Stats : MonoBehaviour {
 	}
 
 	public void resetStats(){
+		Debug.Log ("reset pushed");
 		PlayerPrefs.DeleteKey ("hiscoreP");
 		PlayerPrefs.DeleteKey ("hiscoreB");
 		PlayerPrefs.DeleteKey ("hiscoreT");
+
+		//update stats with current run stats
+		if (player.wonGame) {
+			PlayerPrefs.SetInt ("hiscoreP", endP);
+			PlayerPrefs.SetInt ("hiscoreB", endB);
+			PlayerPrefs.SetFloat ("hiscoreT", endT);
+		}
 
 		int hiscoreP = PlayerPrefs.GetInt ("hiscoreP", 0);
 		int hiscoreB = PlayerPrefs.GetInt ("hiscoreB", 0);
 		float hiscoreT = PlayerPrefs.GetFloat ("hiscoreT", 0f);
 
-		bestStats.text = "Personal Bests\n" + hiscoreT.ToString() + "\n" + hiscoreP.ToString() + "\n" + hiscoreB.ToString();
+		int[] bestTint = minuteSeconds (hiscoreT);
+		string bestT = bestTint[0].ToString()+"m"+bestTint[1].ToString()+"s";
+
+		bestStats.text = "Personal Bests\n" + bestT.ToString() + "\n" + hiscoreP.ToString() + "\n" + hiscoreB.ToString();
 	}
 }
